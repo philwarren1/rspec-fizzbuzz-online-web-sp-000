8030c58f511c3f0680699dc700fbcaa07f201c1f
Philip Warren <philwarren1@gmail.com>
2020-03-25 17:58:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-(1..100).each do |number|
-if (number % 3 == 0) and (number % 5 != 0)
+(1..100).each do |fizz_|
+if (fizz_ % 3 == 0) and (fizz_ % 5 != 0)
 puts "Fizz"
-elsif (number % 5 == 0 ) and (number % 3 != 0)
+elsif (fizz_ % 5 == 0 ) and (fizz_ % 3 != 0)
 puts "Buzz"
-elsif (number % 5 == 0) and (number % 3 == 0)
+elsif (fizz_ % 5 == 0) and (fizz_ % 3 == 0)
 puts "FizzBuzz"
 else
-puts number
+puts fizz_
 end
 end
 end
