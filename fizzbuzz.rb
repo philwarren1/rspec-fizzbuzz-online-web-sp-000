6bdbe0b3abc34a821377afc4e6357f95304aeae5
Philip Warren <philwarren1@gmail.com>
2020-03-25 18:06:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-if (Integer % 3 == 0) and (Integer % 5 != 0)
+  (1..100).map do |number|
+if (number % 3 == 0) and (number % 5 != 0)
 puts "Fizz"
-elsif (Integer % 5 == 0 ) and (Integer % 3 != 0)
+elsif (number % 5 == 0 ) and (number % 3 != 0)
 puts "Buzz"
-elsif (Integer % 5 == 0) and (Integer % 3 == 0)
+elsif (number % 5 == 0) and (number % 3 == 0)
 puts "FizzBuzz"
 else
-puts Integer
+puts number
 end
 end
