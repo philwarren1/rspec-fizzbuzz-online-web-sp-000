717f1765cd1664eb9ad54904e4f9553a8540ed4b
Philip Warren <philwarren1@gmail.com>
2020-03-25 18:02:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-(1..100).each do |Integer|
 if (Integer % 3 == 0) and (Integer % 5 != 0)
 puts "Fizz"
 elsif (Integer % 5 == 0 ) and (Integer % 3 != 0)
